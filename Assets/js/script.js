24bbb7b8de3411acb34a2fec06c41ec02ea60e0b
NithinRG <nithinbiju183@gmail.com>
2020-04-20 15:56:22 +0530

    changed http to https

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,8 @@
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?id=1254163&units=metric&appid=5e04adfe07c1c742dbf5978be28109de", function(data){
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?id=1254163&units=metric&appid=5e04adfe07c1c742dbf5978be28109de", function(data){
     console.log(data);
     let weather = data.weather[0].main;
     let temp = data.main.temp + "°C";
-    let icon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+    let icon = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
     let city = data.name;
     let feel = data.main.feels_like + "°C";
     let wind = data.wind.speed + " m/s";
